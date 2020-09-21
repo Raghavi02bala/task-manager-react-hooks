4aa56e3c06b86bcb836ebbbbaf879d0dc5a9eeff
Raghavi02bala <raghavibalasubramania@gmail.com>
2020-09-21 18:59:40 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import TaskList from './TaskList';
 import "../App.css";
+import TaskListContextProvider from "../context/TaskListContext";
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <TaskListContextProvider>
+      <div className="container">
+        <div className="app-wrapper">
+          <div className="main">
+            <TaskList/>
+          </div>
+        </div>
+      </div>
+    </TaskListContextProvider>
   );
 }
 
