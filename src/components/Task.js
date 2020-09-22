4501490c64568890b3a177adeab988bdc870d87d
Raghavi02bala <raghavibalasubramania@gmail.com>
2020-09-22 12:45:25 +0530

    done

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { TaskListContext } from '../context/TaskListContext';
+
 
 const Task = ({ task }) => {
+
+  const {removeTask} = useContext(TaskListContext);
+
     return (
         <li className="list-item">
       <span>{task.title} </span>
       <div>
         <button
+        // the onClick is retturning a fn named removeTask with argument as task.id
+          onClick={() => removeTask(task.id)}
           className="btn-delete task-btn">
           <i className="fas fa-trash-alt"></i>
         </button>{' '}
